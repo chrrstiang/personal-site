a8908263ecdada57d26112aeaf8b33a391dca531
chrrstiang <garcia.chris@northeastern.edu>
2024-12-18 16:01:00 -0500

    rotate message
    titles now rotate in about page. Working on creating transitions when words change

--- personal-site/src/app/about/page.tsx
+++ personal-site/src/app/about/page.tsx
@@ -1,12 +1,41 @@
+'use client';
 import Image from "next/image";
+import { rotation } from "../../../lib/animations";
+import { useState, useEffect } from "react";
+
 
 export default function About() {
+    
+const [role, setRole] = useState<string>("Student");
+
+const titles : string[] = [
+    "Student",
+    "Powerlifter",
+    "Programmer",
+    "Entrepreneur"
+]
+
+useEffect(() => {
+    let titleIndex = 0;
+
+    // Function to rotate role
+    const rotateRole = () => {
+      setRole(titles[titleIndex]);
+      titleIndex = (titleIndex + 1) % titles.length; // Cycle through titles
+    };
+
+    // Set the interval to call rotateRole every 2 seconds (2000 ms)
+    const intervalId = setInterval(rotateRole, 4000);
+
+    // Clean up the interval when the component is unmounted
+    return () => clearInterval(intervalId);
+  }, []); // Empty dependency array ensures this effect runs once when the component mounts
 
     return(
         <div className="flex flex-col justify-center items-center m-2">
-        <div className="flex flex-col items-center md:m-10 md:justify-evenly md:flex-row-reverse w:1/2">
+        <div className="flex flex-col items-center md:m-10 md:justify-evenly md:flex-row-reverse w:1/2 max-w-screen-md">
         <Image src='/images/my-headshot.webp' alt="headshot of christian garcia" width="350" height="500" />
-        <h1 className="md:w-1/2 text-center text-4xl sm:text-5xl p-5 mb-5 rothwood-bold">Christian is a <span id="role">student</span></h1>
+        <h1 className="md:w-1/2 text-center text-4xl sm:text-4xl p-5 mb-5 rothwood-bold">Christian is a <span id="role">{role}</span></h1>
         </div>
         <div className="flex flex-col justify-center mb-20 items-center m-5 sm:m-0 sm:mb-10 gap-10 lg:max-w-screen-lg">
         <p className="rothwood sm:w-10/12">BIOGRAPHY:Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. 
