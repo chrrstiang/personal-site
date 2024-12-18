24af9e921daa4f9f104b0e63252e40cf6f10eb62
chrrstiang <garcia.chris@northeastern.edu>
2024-12-18 16:28:12 -0500

    end of day: transition not working
    idk what to change but the rotaation works, I was hoping I can apply a CSS class that triggers the animation & use key prop to re mount the component so the transition happens everytime the title changes, no luck yet

--- personal-site/src/app/about/page.tsx
+++ personal-site/src/app/about/page.tsx
@@ -35,7 +35,7 @@ useEffect(() => {
         <div className="flex flex-col justify-center items-center m-2">
         <div className="flex flex-col items-center md:m-10 md:justify-evenly md:flex-row-reverse w:1/2 max-w-screen-md">
         <Image src='/images/my-headshot.webp' alt="headshot of christian garcia" width="350" height="500" />
-        <h1 className="md:w-1/2 text-center text-4xl sm:text-4xl p-5 mb-5 rothwood-bold">Christian is a <span id="role">{role}</span></h1>
+        <h1 className="md:w-1/2 text-center text-4xl sm:text-4xl p-5 mb-5 rothwood-bold">Christian is a <span className="transition-heading-wrapper" id="role" key={role}>{role}</span></h1>
         </div>
         <div className="flex flex-col justify-center mb-20 items-center m-5 sm:m-0 sm:mb-10 gap-10 lg:max-w-screen-lg">
         <p className="rothwood sm:w-10/12">BIOGRAPHY:Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. 
