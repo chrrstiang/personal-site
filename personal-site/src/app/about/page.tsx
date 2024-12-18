f9159da2d3934a912a1028a41189890303a17186
chrrstiang <garcia.chris@northeastern.edu>
2024-12-18 14:18:04 -0500

    finished minimal styling for all pages
    All pages are styled & structured. Web site works for the most part. Now, we implement logic for animations

--- personal-site/src/app/about/page.tsx
+++ personal-site/src/app/about/page.tsx
@@ -6,7 +6,7 @@ export default function About() {
         <div className="flex flex-col justify-center items-center m-2">
         <div className="flex flex-col items-center md:m-10 md:justify-evenly md:flex-row-reverse w:1/2">
         <Image src='/images/my-headshot.webp' alt="headshot of christian garcia" width="350" height="500" />
-        <h1 className="md:w-1/2 text-center text-4xl sm:text-5xl p-5 mb-5 rothwood-bold">Christian is a student</h1>
+        <h1 className="md:w-1/2 text-center text-4xl sm:text-5xl p-5 mb-5 rothwood-bold">Christian is a <span id="role">student</span></h1>
         </div>
         <div className="flex flex-col justify-center mb-20 items-center m-5 sm:m-0 sm:mb-10 gap-10 lg:max-w-screen-lg">
         <p className="rothwood sm:w-10/12">BIOGRAPHY:Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. 
--- personal-site/src/app/contact/page.tsx
+++ personal-site/src/app/contact/page.tsx
@@ -1,17 +1,56 @@
 import Image from "next/image";
+import Link from "next/link";
+
+type LinksProps ={
+    iconLink: string
+    altText: string
+    linkHref: string
+    appName: string
+}
 
 export default function Contact() {
 
     return(
-        <div>
-            <h1>My Links</h1>
-            <div>
-                <span>LinkedIn</span>
-                <span>Instagram</span>
-                <span>Github</span>
-                <span>Email</span>
-                <span>Resume</span>
+        <div className="flex flex-col items-center">
+            <h1 className="m-10 text-6xl rothwood-bold py-8 text-center">My Links</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+                <Links
+                iconLink="/images/ig-logo.webp"
+                altText="Instagram Logo"
+                linkHref="https://www.instagram.com/chrrstiang/"
+                appName="Instagram" />
+                <Links
+                iconLink="/images/github-logo.webp"
+                altText="Github Logo"
+                linkHref="https://github.com/chrrstiang"
+                appName="Github" />
+                <Links
+                iconLink="/images/linkedin-logo.webp"
+                altText="LinkedIn Logo"
+                linkHref="https://www.linkedin.com/in/christian-garcia-7992a5269/"
+                appName="LinkedIn" />
+                <Links
+                iconLink="/images/email-logo.webp"
+                altText="Email Logo"
+                linkHref=""
+                appName="Email" />
+                <Links
+                iconLink="/images/resume-logo.webp"
+                altText="Resume Logo"
+                linkHref=""
+                appName="Resume" />
             </div>
         </div>
     )
 };
+
+function Links({iconLink, altText, linkHref, appName}: LinksProps) {
+    return(
+        <div className="flex flex-row items-center gap-3">
+            <Image src={iconLink} alt={altText} width="30" height="30" />
+            <Link href={linkHref}>
+                <span className="text-3xl rothwood-bold italic">{appName}</span>
+                </Link>
+        </div>
+    )
+}
