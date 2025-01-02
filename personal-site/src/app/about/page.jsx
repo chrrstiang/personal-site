437be263adc48e05e13ed08516edf1b7f79f14c9
chrrstiang <garcia.chris@northeastern.edu>
2025-01-02 13:09:15 -0500

    Fresh Start
    No errors, website is functioning correctly. Will install MUI after this, implement accordions, and move onto different pages

--- personal-site/src/app/about/page.jsx
+++ personal-site/src/app/about/page.jsx
@@ -2,11 +2,6 @@
 import Image from "next/image";
 import { rotation } from "../../../lib/animations";
 import { useState, useEffect } from "react";
-import { CSSTransition } from 'react-transition-group';
-import {Accordion} from '@mui/joy/Accordion';
-import {AccordionDetails} from '@mui/joy/AccordionDetails';
-import {AccordionGroup} from '@mui/joy/AccordionGroup';
-import  { AccordionSummary } from '@mui/joy/AccordionSummary';
 
  
 
@@ -57,12 +52,7 @@ useEffect(() => {
         <div className="flex flex-col items-center md:m-10 md:justify-evenly md:flex-row-reverse w:1/2 max-w-screen-md">
         <Image src={imageUrl} alt="headshot of christian garcia" width="350" height="500" />
         <h1 className="md:w-1/2 text-center text-4xl sm:text-4xl p-5 mb-5 rothwood-bold">
-        Christian is a <CSSTransition
-        in={true}
-        timeout={500}
-        classNames="role"
-        unmountOnExit
-        key={role}><span>{role}</span></CSSTransition>
+        Christian is a <span>{role}</span>
             </h1>
         </div>
         <div className="flex flex-col justify-center mb-20 items-center m-5 sm:m-0 sm:mb-10 gap-10 lg:max-w-screen-lg">
