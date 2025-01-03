845bd272582ef131e46357e8ee4067588a9ce777
chrrstiang <garcia.chris@northeastern.edu>
2025-01-03 18:13:52 -0500

    error currently with tailwind
    says 'unknown at rule @tailwind'

--- personal-site/src/app/about/page.jsx
+++ personal-site/src/app/about/page.jsx
@@ -94,7 +94,7 @@ function AboutHeader({imageUrl, role}) {
     return(
     <div className="flex flex-col items-center md:m-10 md:justify-evenly md:flex-row-reverse w:1/2 max-w-screen-md">
         <Image src={imageUrl} alt="headshot of christian garcia" width="350" height="500" />
-        <h1 className="md:w-1/2 text-center text-4xl sm:text-4xl p-5 mb-5 rothwood-bold">
+        <h1 className="md:w-1/2 text-center text-4xl sm:text-4xl p-5 mb-5 abril">
         Christian is a <span>{role}</span>
         </h1>
     </div>
